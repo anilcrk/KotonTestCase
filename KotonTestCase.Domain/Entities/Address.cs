afcb393d96fe1a93053cc82827ead13959ba32e0
anil.cirik <anil.cirik@W10N-ANIL>
2022-04-18 01:32:42 +0300

    added entities

---
+++ KotonTestCase.Domain/Entities/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotonTestCase.Domain.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// full address
+        /// </summary>
+        public string Description { get; set; }
+
+        //Relations
+        public Customer Customer { get; set; }
+    }
+}
